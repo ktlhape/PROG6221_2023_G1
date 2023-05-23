0682adf5c804042264d0a07523fe5f0f7d5b9134
ktlhape <ktlhape@iie.ac.za>
2023-05-23 13:10:37 +0200

    Introduction to Delegates

---
+++ DelegatesIntro/DelegatesIntro/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesIntro
+{
+    internal class Program
+    {
+        //declare a public delegate
+        public delegate void OutputDelegate(string msg);
+        static void Main(string[] args)
+        {
+            //Print("Hello World");
+            //OutputDelegate printDelegate = new OutputDelegate(Print);
+            OutputDelegate printDelegate = Print;
+            OutputDelegate displayDelegate = Display;
+
+            printDelegate("Hello World - using a Delegate");
+            displayDelegate("Kabelo");
+
+            Console.Read();
+        }
+
+        static void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+        static void Display(string text)
+        {
+            Console.WriteLine(text);
+        }
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ UsingDelegates/UsingDelegates/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingDelegates
+{
+    internal class Program
+    {
+        delegate bool FilterDelegate(int x);
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 3,76,35,-13,-5,18,-34,91,15,20};
+
+            FilterDelegate even = IsEven;
+            Display(numbers, even);
+            Console.WriteLine("+++++++++++++++++++");
+           
+        }
+
+        static bool IsEven(int x)
+        {
+            return x % 2 == 0;
+        }
+
+        static bool IsOdd(int x)
+        {
+            return x % 2 != 0;
+        }
+        static void Display(List<int> ar, FilterDelegate filter)
+        {
+            foreach (int item in ar)
+            {
+                if (filter(item))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
