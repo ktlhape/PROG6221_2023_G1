40f58aa9e2d026334166a6f9550af8e2d48107dd
ktlhape <ktlhape@iie.ac.za>
2023-05-30 14:56:19 +0200

    Unit test project added. Test  methods from the person calss

---
+++ UnitTsting/PersonTester/PersonUnitTest.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UnitTsting;
+
+namespace PersonTester
+{
+    [TestClass]
+    public class PersonUnitTest
+    {
+        [TestMethod]
+        public void TestSum()
+        {
+            //Arrange
+            int num1 = 5;
+            int num2 = 7;
+            int expected = 12;
+
+            //Act
+            int actual = num1 + num2;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestCanVote()
+        {
+            //Arrange
+            int age = 5;
+            bool expected = false;
+
+            //Act
+            Person p = new Person("Kabelo", "Tlhape", age);
+            bool actual = p.CanVote();
+            //Assert
+            Assert.AreEqual(expected, actual);
+            //Assert.IsTrue(expected.Equals(actual));
+        }
+        [TestMethod]
+        public void TestLength()
+        {
+            //Arrange
+            string name = "Kabelo";
+            int expected = 6;
+
+            //Act
+            Person p = new Person(name, "Smit", 12);
+            int actual = p.StringLength(name);
+
+            //Assert
+            Assert.IsTrue(expected == actual);
+
+        }
+    }
+}
---
+++ UnitTsting/UnitTsting/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTsting
+{
+    public class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+
+        public Person(string firstname, string lastname, int age)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+            Age = age;
+        }
+
+        public bool CanVote()
+        {
+            return Age <= 18;
+        }
+
+        public int StringLength(string name)
+        {
+            return name.Length ;
+        }
+    }
+}
